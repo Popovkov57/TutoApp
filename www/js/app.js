3496b3f7d467f01e557bdcc423608b25a8955591
Kevin <kevin.patrzek@live.fr>
2015-01-08 17:24:31 +0100

    First commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,9 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var starter = angular.module('starter', ['ionic']);
 
-starter.run(function($ionicPlatform) {
+var serviceApp = angular.module('serviceApp', ['ionic']);
+
+serviceApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,12 +16,7 @@ starter.run(function($ionicPlatform) {
   });
 })
 
-starter.controller('controller', function($scope){
-    //console.log("Hello");
-    $scope.custumers=[
-        {name:'Max', city:'Paris'},
-        {name:'Jojo', city:'Paris'},
-        {name:'Roger', city:'Metz'},
-        {name:'Max', city:'Paris'}
-    ];
-})
+serviceApp.controller('serviceAppController', ['$scope', function($scope){
+    console.log("hello world");
+}])
+
