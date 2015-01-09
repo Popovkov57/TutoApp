fdbb77ee1e674c1ba49606c6980ce7b92b7360a2
Kevin <kevin.patrzek@live.fr>
2015-01-09 14:07:33 +0100

    Exo/1 fini - Affichage des users

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,10 @@
-// Exo 1
-// lien => https://gist.github.com/skelz0r/80a41c9ef24b16879c3a
-
-
 var serviceApp = angular.module('serviceApp', ['ionic']);
 
 var users = [];
 
 serviceApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -21,33 +16,26 @@ serviceApp.run(function($ionicPlatform) {
 
 serviceApp.factory('Users', function($http) {
     
-    
+    var users = []; 
     
     var init = function(){
         
-        // load 5 ramdom person 
         $http.get('http://api.randomuser.me/?results=5').success(function(data){
-        
-        users = data;
          
-        // parcours la liste data et
-        // récupère les données de chaque utilisateur
         for(var i=0; i<data.results.length; i++){
-             
-        users = {firstname:data.results[i].user.name.first,
-                 lastname:data.results[i].user.name.last,
-                 email:data.results[i].user.email,
-                 city:data.results[i].user.location.city,
-                 street:data.results[i].user.location.street};
-        }
-        
-        //console.log(users);
+            users[i]= 
+                {"firstname": data.results[i].user.name.first,
+                 "lastname": data.results[i].user.name.last,
+                 "email": data.results[i].user.email,
+                 "city": data.results[i].user.location.city,
+                 "street": data.results[i].user.location.street
+                };
+        };
             
         });
     }
     
     var getList = function() {
-        console.log(users);
         return users;
     }
     
@@ -56,7 +44,6 @@ serviceApp.factory('Users', function($http) {
         getList: getList
     }
 });
-
 
 serviceApp.controller('serviceAppController', function($scope, Users){
     Users.init();
