70b400c129c17b3be6376b38cd6102a6d8872fad
Kevin <kevin.patrzek@live.fr>
2015-01-12 09:31:16 +0100

    Liaison entre add() et Button ok

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ var users = [];
 
 serviceApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -14,19 +14,19 @@ serviceApp.run(function($ionicPlatform) {
   });
 })
 
-serviceApp.factory('Users', function($http) {
-    
-    var users = []; 
-    
+serviceApp.factory('Users',function($http) {
+
+    var users = [];
+
     var init = function(){
-        
+
         $http.get('http://api.randomuser.me/?results=5').success(function(data){
-         
+
             for(var i=0; i<data.results.length; i++){
 
                 var tempUser = data.results[i].user;
 
-                users[i]= 
+                users[i]=
                     {"firstname": tempUser.name.first,
                      "lastname": tempUser.name.last,
                      "avatarURL": tempUser.picture.thumbnail,
@@ -34,20 +34,26 @@ serviceApp.factory('Users', function($http) {
                      "street": tempUser.location.street
                     };
             };
-            
+
         });
     }
-    
-    var add = function(){
-        
-        console.log(users);
-    
+
+    var add = function($scope){
+
+      $scope.push = function() {
+        console.log("Je suis là !!");
+      }
+
     }
-    
+
+
+
+
+
     var getList = function() {
         return users;
     }
-    
+
     return {
         init: init,
         add: add,
@@ -55,8 +61,12 @@ serviceApp.factory('Users', function($http) {
     }
 });
 
-serviceApp.controller('serviceAppController', function($scope, Users){
+
+
+
+
+serviceApp.controller('usersCtrl', function($scope, Users){
     Users.init();
-    Users.add();
     $scope.users = Users.getList();
-});   
+    Users.add($scope);
+});
