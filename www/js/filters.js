71f358230bb4eb1c4b6ee3465ceb05d1db4edfc8
Kevin <kevin.patrzek@live.fr>
2015-01-20 18:14:05 +0100

    refactor filter.js

--- www/js/filters.js
+++ www/js/filters.js
@@ -2,24 +2,8 @@ angular.module('serviceApp')
 
 .filter('fullName', function(){
   return function(user){
-
     var fullname = user.firstname + ' ' + user.lastname;
 
-    if(fullname == ' '){
-
-      if(user.email != ''){
-
-        return user.email;
-
-      }
-
-      return "anonymous";
-
-    }else{
-
-      return (fullname.replace(/\s$/, "").replace(/^\s/, ""));
-
-    }
-
+    return (fullname.trim() || user.email || 'anonymous');
   }
 })
