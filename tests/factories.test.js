f34a48438bcbc5552ba730707b31ad1175c1806a
Kevin <kevin.patrzek@live.fr>
2015-01-20 20:32:38 +0100

    Add a dummy functionnel test for Users factory

---
+++ tests/factories.test.js
@@ -0,0 +1,25 @@
+describe('Users', function(){
+  var httpMock, users, timeout;
+  var data = [];
+
+  beforeEach(module('serviceApp.factories'));
+
+  beforeEach(inject(function ($httpBackend, Users, $timeout) {
+    httpMock = $httpBackend;
+    users = Users;
+    timeout = $timeout;
+  }));
+
+  it("should get users from API", function () {
+
+    httpMock.expectGET("http://api.randomuser.me/?results=5").respond({
+      results: data
+    });
+
+    users.getList().then( function(users){
+      expect(users).toEqual([]);
+    });
+
+    httpMock.flush();
+  });
+});
